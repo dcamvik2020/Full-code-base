68204677519912a50113d359677ffe1d65cb08ee
Victor <vik-melnikov2001@mail.ru>
2021-04-12 12:01:09 +0300

    removed some directories

--- IT/Codeblocks/Acronis/ForAisha/Huffman/String.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-
-void Reverse(std::string& tmp);
-int my_strcmp(const std::string& s1, const std::string& s2);
-int my_strncmp(const std::string& s1, const std::string& s2 , unsigned len);
-bool is_null(const std::string& s);
